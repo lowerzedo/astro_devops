7fed89aa5b914f320ad48468be6cf1d2eb6bb69d
Nazar Hasanov <tp052475@mail.apu.edu.my>
2024-10-27 03:08:34 +0800

    fix: aws route private

--- vpc.tf
+++ vpc.tf
@@ -82,9 +82,10 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route" "private_nat_gateway" {
+  for_each               = aws_subnet.private
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat[*].id[0]
+  nat_gateway_id         = aws_nat_gateway.nat[each.key].id
 }
 
 resource "aws_route_table_association" "private" {
