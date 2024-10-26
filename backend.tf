697a4a2e29b46de61489e2f2dcda0d6e12e2bc72
Nazar Hasanov <tp052475@mail.apu.edu.my>
2024-10-27 02:27:22 +0800

    fix: change of region for s3 bucket

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 # Need to create that S3 bucket to store the Terraform state file. So later we can use tf destroy to remove all the resources we created.
 terraform {
   backend "s3" {
-    region = "ap-southeast-5"
-    bucket = "nazar-temp-terraform-state"
+    region = "ap-southeast-1"
+    bucket = "nazar-astro-terraform-state"
     key    = "terraform.tfstate"
   }
 }
